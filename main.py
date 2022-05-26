e5a456d6101e320a3768b4f3f88666bd06da2495
Mechanic <gazizovrustam.r@gmail.com>
2022-05-26 17:50:33 +0300

    init_2

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 def main():
+    fatGram = float(input("Please enter fat: "))
+    carbohydratesGram = float(input("Please enter carbohydrates: "))
+    caloriesFromFat = getCaloriesFromFat(fatGram)
+    caloriesFromCarbohydrates = getCaloriesFromCarbohydrates(carbohydratesGram)
+    print(caloriesFromFat, caloriesFromCarbohydrates)
+
+
+def getCaloriesFromFat(gram):
+    return gram * 9
+
+def getCaloriesFromCarbohydrates(gram):
+    return gram * 4
+
+
+main()
